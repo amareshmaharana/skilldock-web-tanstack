6942543b7f02b8c5d985c4e6512c10ca258691ca
Amaresh Maharana <amareshcoder@gmail.com>
2026-05-15 14:04:47 +0530

    add: implement Navbar component for user authentication and navigation

---
+++ src/components/Navbar.tsx
@@ -0,0 +1,34 @@
+import { Show, UserButton } from "@clerk/tanstack-react-start";
+import { Link } from "@tanstack/react-router";
+import { LogIn } from "lucide-react";
+
+const Navbar = () => {
+  return (
+    <nav className="navbar">
+      <div className="brand">
+        <div className="mark">
+          <div className="glyph" />
+        </div>
+
+        <Link to="/">
+          <span>SkillDock</span>
+        </Link>
+      </div>
+
+      <div className="actions">
+          <Show when="signed-in">
+            <UserButton />
+          </Show>
+
+        <Show when="signed-out">
+          <Link to="/sign-in/$" className="btn-primary">
+            <LogIn size="16" />
+            Sign In
+          </Link>
+        </Show>
+      </div>
+    </nav>
+  );
+};
+
+export default Navbar;
