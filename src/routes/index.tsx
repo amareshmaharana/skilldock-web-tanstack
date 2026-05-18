04d0b594bb6521bf5ff57c955a2aac3878136d31
Amaresh Maharana <amareshcoder@gmail.com>
2026-05-18 18:41:52 +0530

    add: implement skills fetching and update Home component for dynamic data display

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,11 +1,36 @@
 import { createFileRoute, Link } from "@tanstack/react-router";
 import { Bot, Terminal } from "lucide-react";
 import SkillCard from "#/components/SkillCard";
-import dummySkills from "#/lib/dummy-skill";
+import { createServerFn } from "@tanstack/react-start";
+import { getSkills } from "#/dataconnect-generated";
+import { dataConnect } from "#/lib/firebase";
+import { usePostHog } from "posthog-js/react";
 
-export const Route = createFileRoute("/")({ component: Home });
+const getSkillsFn = createServerFn({ method: "GET" }).handler(async () => {
+  try {
+    const { data } = await getSkills(dataConnect, {
+      searchTerm: "",
+      limit: 10,
+    });
 
-function Home() {
+    return data.skills;
+  } catch (error) {
+    console.error("Error fetching skills:", error);
+    return [];
+  }
+});
+
+export const Route = createFileRoute("/")({
+	component: App,
+	loader: () => getSkillsFn(),
+});
+
+function App() {
+  const posthog = usePostHog();
+
+  const skills = Route.useLoaderData();
+  // const skills = Array.isArray(loaderData) ? loaderData : loaderData?.skills ?? [];
+
   return (
     <div id="home">
       {/* ++++++++ HERO ++++++++ */}
@@ -24,12 +49,20 @@ function Home() {
         </div>
 
         <div className="actions">
-          <Link to="/skills" className="btn-primary">
+          <Link
+            to="/skills"
+            className="btn-primary"
+            onClick={() => posthog.capture("browse_registry_clicked")}
+          >
             <Terminal size="16" />
             <span>Browse Registry</span>
           </Link>
 
-          <Link to="/skills/new-skill" className="btn-secondary">
+          <Link
+            to="/skills/new-skill"
+            className="btn-secondary"
+            onClick={() => posthog.capture("publish_skill_clicked")}
+          >
             <Bot size="16" />
             <span>Publish Skill</span>
           </Link>
@@ -38,8 +71,8 @@ function Home() {
 
       {/* ++++++++ LATEST ++++++++ */}
       <section className="latest">
-        <div className="space-y-2">
-          <h2>
+        <div className="space-y-2 text-center">
+          <h2 className="text-4xl">
             Latest <span className="text-gradient">Innovations</span>
           </h2>
 
@@ -51,14 +84,14 @@ function Home() {
         </div>
 
         <div>
-          {dummySkills.length > 0 ? (
+          {skills.length > 0 ? (
             <div className="skills-grid">
-              {dummySkills.map((skill) => (
-                <SkillCard key={skill.id} skill={skill} />
+              {skills.map((skill) => (
+                <SkillCard key={skill.id} {...skill} />
               ))}
             </div>
           ) : (
-            <p>You got nothing to see here about the innovations.</p>
+            <p className="text-center font-bold">You got nothing to see here about the innovations.</p>
           )}
         </div>
       </section>
